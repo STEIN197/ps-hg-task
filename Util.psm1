4508f9ec1be74661e7f2d0ab84b9005cf5dc3c37
Nail Gafarov <nil20122013@gmail.com>
2021-03-18 16:32:13 +0500

    Switched to functional approach

--- Util.psm1
+++ Util.psm1
@@ -4,3 +4,56 @@ class Util {
 		return $String -match '^\d+$';
 	}
 }
+
+function Is-Numeric([string] $string) {
+	return $string -match '^\d+$'
+}
+
+function Hg-Installed {
+	try {
+		hg > $null
+		return $true
+	} catch {
+		return $false
+	}
+}
+
+function Hg-Bookmarks {
+	return (hg bookmarks --template '{bookmark}\n') -split '^n'
+}
+
+function Hg-Active-Bookmark {
+	return hg log -r . --template '{activebookmark}'
+}
+
+function Hg-Current-Path {
+	$currentItem = Get-Item -Path .;
+	while ($currentItem -ne $null -and !(Test-Path "$($currentItem.FullName)\.hg")) {
+		$currentItem = $currentItem.Parent
+	}
+	return $currentItem ?? $currentItem.FullName
+}
+
+function Config-Exists {
+	return Test-Path -Path "$($PSScriptRoot)\config.json"
+}
+
+function Config-Create {
+	New-Item -Path $PSScriptRoot -Name 'config.json' -ItemType 'file'
+}
+
+function Config-Path {
+	return "$($PSScriptRoot)\config.json"
+}
+
+function Config-Get {
+	return Get-Content -Path Config-Path -Raw | ConvertFrom-Json -AsHashtable ?? @{
+		repositories = @{}
+	}
+}
+
+function Config-Save([hashtable] $data) {
+	$data | ConvertTo-Json -Depth 16 | Out-File -FilePath Config-Path;
+}
+
+Export-ModuleMember -Function *
--- main.ps1
+++ main.ps1
@@ -1,21 +1,20 @@
-using module .\Task.psm1;
-using module .\Config.psm1;
-using module .\Mercurial.psm1;
-using module .\Util.psm1;
-using module .\CLI.psm1;
+# using module .\Task.psm1;
+# using module .\Config.psm1;
+# using module .\Mercurial.psm1;
+# using module .\Util.psm1;
+# using module .\CLI.psm1;
+
+Import-Module .\Util.psm1
 
 function Main {
-	if (![Mercurial]::Installed()) {
-		throw "Mercurial is not installed on this computer";
-	}
-	if (![Mercurial]::Current()) {
-		throw "Mercurial repository does not exist here";
-	}
+	Check-Setup
+	return
 	if (!$Args.Length) {
-		throw "No arguments provided";
-	}
-	if (![Config]::Exists()) {
-		[Config]::Create();
+		[Task] $CurrentTask = [Task]::Current();
+		if ($CurrentTask) {
+			Write-Host "$($CurrentTask)`t$($CurrentTask.Description)";
+		}
+		return;
 	}
 	[string] $Action = $Args[0];
 	[string] $ActionRegex = $Action -split '' -match '[a-z]' -join '.*';
@@ -30,6 +29,37 @@ function Main {
 		throw "Unknown operation `"$($Action)`"";
 	}
 	[CLI]::$Action($Args[1..$Args.Length]);
+}
+
+function Check-Setup {
+	if (!(Hg-Installed)) {
+		throw "Mercurial is not installed on this computer";
+	}
+	if (!(Hg-Current-Path)) {
+		throw "Mercurial repository does not exist here";
+	}
+	if (!(Config-Exists)) {
+		Config-Create
+	}
+	[hashtable] $repoConfig = (Config-Get).repositories[(Hg-Current-Path)]
+	if (!$repoConfig.bookmark) {
+		throw "Main bookmark property is unset";
+	}
+	if (!(Hg-Bookmarks -match $repoConfig.bookmark)) {
+		throw "Main bookmark $($RepoConfig.bookmark) does not exist here";
+	}
+	if (!$repoConfig.branch) {
+		throw "Branch property is unset";
+	}
+	# (hg bookmarks -T '{bookmarks},{branch}\n') -split '^n' | % {
+	# 	$Book, $Branch = $_ -split ',';
+	# 	if ($Book -eq $RepoConfig.bookmark -and $Branch -ne $RepoConfig.branch) {
+	# 		throw "Bookmark `"$($Book)`" does not belong to `"$($RepoConfig.branch)`"";
+	# 	}
+	# }
+	# if (!$RepoConfig.prefix) {
+	# 	throw "Prefix property is unset";
+	# }
 }
 
 try {
